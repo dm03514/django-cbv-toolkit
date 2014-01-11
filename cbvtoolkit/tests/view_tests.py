646390e6af7f286129f086ddaa78755de80c6fae
Daniel <daniel@daniel.(none)>
2014-01-11 12:50:25 -0500

    Testing Skeleton, uses django_nose though

---
+++ cbvtoolkit/tests/view_tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+class MyTestTestCase(TestCase):
+
+    def test_something(self):
+        self.assertEqual(2, 2)
---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name = "cbvtoolkit",
+    version = "0.1dev",
+    packages = ['cbvtoolkit'],
+)
+
